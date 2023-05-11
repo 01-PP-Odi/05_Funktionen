706ef3d23dde14c7fe18087638ce3e99bf3f4b48
Odirax <dario.lucic@hotmail.de>
2023-05-11 10:16:31 +0200

    // 2a. Parametrisierung + Datenübergabe von INNEN

--- script_01.js
+++ script_01.js
@@ -3,3 +3,29 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration 
+function test()
+{
+    console.log("Hallo Karsten!");
+}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+
+function ausgabeNamen() 
+{
+    // what happens in VEGAS ... alles in der function bleibt gekapselt.
+    let firstName = "Karsten";
+    console.log("Hallo " + firstName + "!");
+
+
+
+}
+
+// console.log(firstName); // Fehler: Scope!
